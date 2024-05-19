3358a599898d432ceb0bcabc03c512ddb2326dab
PuxHocDL <161716393+PuxHocDL@users.noreply.github.com>
2024-05-19 22:16:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,23 +6,21 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tower Defense Game")
 
 # Màu sắc
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 PURPLE = (128, 0, 128)  # Màu của vật làm chậm
-
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 128, 0)
+mediumaquamarine = (102, 205, 170)
 # Tốc độ khung hình
 FPS = 60
 
 # Kích thước quái vật và đạn
-MONSTER_SIZE = 50
 BULLET_SIZE = 10
 TOWER_SIZE = 50
 SLOW_SIZE = 50
-
 # Tốc độ đạn
 bullet_speed = 5
 # Giá tiền mua tháp và vật làm chậm
@@ -45,6 +43,7 @@ font = pygame.font.SysFont(None, 36)
 placing_tower = False
 placing_slow = False
 # Tạo danh sách chứa các quái vật, đạn, tháp, và vật làm chậm
-monsters = []
+BUGs = []
 bullets = []
 towers = []
+
