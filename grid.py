812198dbc9f1cd1ff6354c19ecbb1df35ef2261a
GenyoNguyen <huydragon234@gmail.com>
2024-05-22 22:38:24 +0700

    Renamed self.__cells to self.__objects, which contains all Towers variable with respective grid positions, justified related methods.

--- grid.py
+++ grid.py
@@ -5,7 +5,7 @@ class Cell:
     """
     Class for Cell object.
     """
-    def __init__(self, x, y, image=None):
+    def __init__(self, x, y, object, image=None):
         """
         Initializes the Cell object.
 
@@ -15,6 +15,7 @@ class Cell:
         self.__x = x
         self.__y = y
         self.__image = image
+        self.__object = object
 
     def get_image(self):
         """
@@ -27,6 +28,9 @@ class Cell:
         Returns Cell's position on the Grid.
         """
         return self.__x, self.__y
+
+    def get_object(self):
+        return self.__object
 
 
 class Grid:
@@ -50,15 +54,15 @@ class Grid:
         self.__cols = 9
         self.__imgs = imgs
         self.__bg_img = bg_img
-        self.__cells = [[None for _ in range(self.__cols)] for _ in range(self.__rows)]
+        self.__objects = []
 
     def reset(self):
         """
         Resets all the objects.
         """
-        self.__cells = []
+        self.__objects = []
 
-    def add_object(self, x, y, image=None):
+    def add_object(self, x, y, object, image=None):
         """
         Add an object to a Cell on the Grid.
 
@@ -67,9 +71,9 @@ class Grid:
             y (int): the y position on the Grid.
             image (pygame.Image): the image to be drawn.
         """
-        self.__cells[x][y] = Cell(x, y, image)
+        self.__objects.append(Cell(x, y, object, image))
 
-    def convert(self, x, y):
+    def convert_to_grid_pos(self, x, y):
         """
         Converts xy screen position to xy Grid position if possible.
 
@@ -80,12 +84,24 @@ class Grid:
         Returns:
             (x, y): the xy position of the Grid. (-1, -1) if the passed position is beyond Grid's position or the Cell already has an object.
         """
-       # if x < 50 or x > 50 + self.__size*self.__cols or y > self.__screen_height - 50 or y < self.__screen_height - 50 - self.__size*self.__rows:
-       #     return -1, -1
         x, y = (y - (self.__screen_height - 50 - self.__size*self.__rows))//self.__size, (x-50)//self.__size
-        if x in range(self.__cols) and y in range(self.__rows) and not self.__cells[x][y]:
+        if y in range(self.__cols) and x in range(self.__rows) and not (x, y) in self.get_objects_pos():
             return x, y
         return -1, -1
+    
+    def convert_to_screen_pos(self, i, j):
+        """
+        Converts xy Grid position to xy screen position.
+
+        Parameters:
+            i (int): the row number in Grid.
+            j (int): the column number in Grid.
+        """
+        x_offset = 50
+        y_offset = self.__screen_height - 50 - self.__size*self.__rows
+        x = x_offset + j*self.__size
+        y = y_offset + i*self.__size
+        return x, y
 
 
     def draw(self, screen):
@@ -106,9 +122,33 @@ class Grid:
                 pygame.draw.rect(screen, colors.gray, [x, y, self.__size, self.__size], 1)
                 img_counter += 1
 
-                if self.__cells[i][j] is not None:
-                    cell_img = self.__cells[i][j].get_image()
-                    if cell_img:
-                        screen.blit(cell_img, (x, y))
-                    else:
-                        pygame.draw.rect(screen, colors.red, [x, y, self.__size, self.__size])
+        for cell in self.__objects:
+            pos = cell.get_pos()
+            screen_pos = self.convert_to_screen_pos(pos[0], pos[1])
+            cell_img = cell.get_image()
+            if cell_img:
+                screen.blit(cell_img, screen_pos)
+            else:
+                pygame.draw.rect(screen, colors.red, [screen_pos[0], screen_pos[1], self.__size, self.__size])
+
+    def draw_on_mouse_pos(self, screen, pos, img=None):
+        """
+        Draws the image on the mouse position.
+
+        Parameters:
+            screen (pygame.Surface): the surface to be drawn on.
+            pos (tuple): xy mouse position.
+        """
+        if img:
+            screen.blit(img, (pos[0], pos[1]))
+        else:
+            pygame.draw.rect(screen, colors.red, [pos[0]-self.__size//2, pos[1]-self.__size//2, self.__size, self.__size])
+
+    def get_cell_size(self):
+        return self.__size
+
+    def get_objects(self):
+        return [object.get_object() for object in self.__objects]
+
+    def get_objects_pos(self):
+        return [object.get_pos() for object in self.__objects]
