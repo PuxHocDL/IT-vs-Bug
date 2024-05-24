e878989b95f1ed8db54929528f5d883757b8d93b
PuxHocDL <161716393+PuxHocDL@users.noreply.github.com>
2024-05-25 06:59:45 +0700

    Update image!

--- bullet.py
+++ bullet.py
@@ -1,6 +1,21 @@
 import pygame
 from config import *
+
 class Bullet:
-     # Hàm vẽ đạn
-    def draw_bullet(x, y):
-        pygame.draw.rect(screen, BLACK, (x, y, BULLET_SIZE, BULLET_SIZE))
+    
+    normal_bullet_image = pygame.image.load(os.path.join("assets", "PeaNormal_0.png"))
+
+    break_bullet_image = pygame.image.load(os.path.join("assets", "PeaNormalExplode_0.png"))
+
+    ice_bullet_image = pygame.image.load(os.path.join("assets", "PeaIce_0.png"))
+
+    @staticmethod
+    def draw_normal_bullet(x, y):
+            screen.blit(Bullet.normal_bullet_image, (x, y))     
+
+    def draw_ice_bullet(x, y):
+            screen.blit(Bullet.ice_bullet_image, (x, y))
+
+    def draw_bullet_with_collision(x,y): 
+            screen.blit(Bullet.break_bullet_image, (x, y))
+    
