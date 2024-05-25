f266e166705ff983fae30ab4527996761be79764
PuxHocDL <161716393+PuxHocDL@users.noreply.github.com>
2024-05-25 10:22:30 +0700

    Update tower.py

--- tower.py
+++ tower.py
@@ -13,31 +13,34 @@ class BasicTower:
         self.tower_type = "Tower"
         self.health=100
         self.image=None
-    def create_basic_tower(x, y): 
-        towers.append(BasicTower(x, y))
-    def upgrade(index):
+        
+    def upgrade(self):
         global gold
         """Nâng cấp tháp"""
-        if gold.gold >= upgrade_cost and towers[index].level < 3:
-            towers[index].level += 1
+        if gold.gold >= upgrade_cost and self.level < 3:
+            self.level += 1
             gold.gold -= upgrade_cost
+            
     def shoot(self):
         """Tháp bắn đạn"""
-        bullet_x = self.x + TOWER_SIZE // 2 - BULLET_SIZE // 2
-        bullet_y = self.y + TOWER_SIZE // 2 - BULLET_SIZE // 2
+        bullet_x = self.x - BULLET_SIZE // 2
+        bullet_y = self.y - BULLET_SIZE // 2
+        
         if self.level == 1:
-            bullets.append([bullet_x, bullet_y, 0, self.level, "normal"])  # Đạn bình thường
+             bullets.append([bullet_x, bullet_y, 0, self.level, "normal"])  # Đạn bình thường
+
         elif self.level == 2:
-            bullets.extend([[bullet_x, bullet_y, angle, self.level, "normal"] for angle in [-0.2, 0, 0.2]])  # Đạn cuồng cung
+             bullets.extend([[bullet_x, bullet_y, angle, self.level, "normal"] for angle in [-0.2, 0, 0.2]])  # Đạn cuồng cung
+            
         else:
             if bugs:
                 nearest_bug = min(bugs, key=lambda m: math.hypot(m.x - bullet_x, m.y - bullet_y))
                 angle = math.atan2(nearest_bug.y - bullet_y, nearest_bug.x - bullet_x)
             else:
                 angle = 0
-            bullets.append([bullet_x, bullet_y, angle, self.level,"normal"])
-            bullets.append([bullet_x, bullet_y, angle, self.level,"normal"])
-            bullets.append([bullet_x, bullet_y, angle, self.level,"normal"])  # Đạn đuổi
+            bullets.append([bullet_x, bullet_y, angle, self.level, "normal"])  # Đạn đuổi
+            bullets.append([bullet_x, bullet_y, angle, self.level, "normal"])  # Đạn đuổi
+            bullets.append([bullet_x, bullet_y, angle, self.level, "normal"])  # Đạn đuổi
 
 class SlowTower(BasicTower):
     """Tháp làm chậm, kế thừa từ basic_tower, làm chậm tốc độ di chuyển của quái"""
@@ -68,20 +71,10 @@ class TowerGame:
         """Nâng cấp tháp tại vị trí index"""
         BasicTower.upgrade(index)
     
-    def draw_buy_tower_button():
-        """Vẽ nút mua tháp"""
-        BasicTower.draw_buy_button()
-    def draw_buy_ice_button():
-        """Vẽ nút mua tháp băng"""
-        tower_ice.draw_buy_ice_button()
-    
-    def draw_buy_slow_button():
-        """Vẽ nút mua vật làm chậm"""
-        SlowTower.draw_buy_slow_button()
-    
     def draw_slow(x, y):
         """Vẽ vật làm chậm ở vị trí x, y"""
         pygame.draw.rect(screen, PURPLE, (x, y, TOWER_SIZE, TOWER_SIZE))
+        
 class tower_ice(BasicTower):
     """Tháp băng, bắn đạn gây sát thương và làm chậm kẻ địch"""
     def __init__(self, x, y):
@@ -89,13 +82,11 @@ class tower_ice(BasicTower):
         self.cost = 150
         self.color_for_levels = {1: LIGHT_BLUE, 2: ICE_BLUE, 3: DARK_BLUE}
         self.tower_type = "Ice"
-        
-    def create_ice_tower(x, y): 
-        towers.append(tower_ice(x, y))
+
     def shoot(self):
         """Tháp băng bắn đạn làm chậm kẻ địch"""
-        bullet_x = self.x + TOWER_SIZE // 2 - BULLET_SIZE // 2
-        bullet_y = self.y + TOWER_SIZE // 2 - BULLET_SIZE // 2
+        bullet_x = self.x - BULLET_SIZE // 2
+        bullet_y = self.y - BULLET_SIZE // 2
         if self.level == 1:
             bullets.append([bullet_x, bullet_y, 0, self.level, "ice"])  # Đạn băng bình thường
         elif self.level == 2:
@@ -110,8 +101,8 @@ class tower_ice(BasicTower):
             bullets.append([bullet_x, bullet_y, angle, self.level, "ice"])
             bullets.append([bullet_x, bullet_y, angle, self.level, "ice"])  # Đạn băng đuổi
             
-    def upgrade(index):
+    def upgrade(self):
         """Nâng cấp tháp"""
-        if gold.gold >= upgrade_cost and towers[index].level < 3:
-            towers[index].level += 1
+        if gold.gold >= upgrade_cost and self.level < 3:
+            self.level += 1
             gold.gold -= upgrade_cost
