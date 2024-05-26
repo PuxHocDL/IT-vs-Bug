5fce938c2520251c31ab4a17a1bf6a9e7bd10715
GenyoNguyen <huydragon234@gmail.com>
2024-05-26 11:22:21 +0700

    Added class for managing bugs.

---
+++ bug_manager.py
@@ -0,0 +1,31 @@
+from bug import *
+
+class BugManager:
+    __bug_types = {"NormalBug": NormalBug, "BigBug": BigBug, "TriangleBug": TriangleBug, "HexagonBug": HexagonBug}
+    def __init__(self):
+        self.__bugs = []
+
+    def add_bug(self, grid, name):
+        """
+        Static method to create and add a big bug to the game.
+
+        Parameters:
+            grid (Grid): The grid object to determine the bug's starting position.
+            name (string): Name of the bug.
+        """
+        bug_x = WIDTH
+        bug_y = random.choice(range(HEIGHT - 50 - grid.get_cell_size() * grid.get_rows(), HEIGHT - 50 - grid.get_cell_size(), grid.get_cell_size())) + (grid.get_cell_size()-80)//2 + 70
+        self.__bugs.append(BugManager.__bug_types[name](bug_x, bug_y, 0.5, 1000, 1000, 80, 150, 150, name=name))
+
+    def get_bugs(self):
+        return self.__bugs
+
+    def get_bugs_pos(self):
+        return [[bug.get_x(), bug.get_y()] for bug in self.__bugs]
+
+    def remove_bug(self, bug):
+        self.__bugs.remove(bug)
+
+    def apply_slow_effect(self):
+        for bug in self.__bugs:
+            bug.apply_slow()
