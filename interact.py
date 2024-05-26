262629d779c8bf47e08bc5c3d6d044267cd82a86
PuxHocDL <161716393+PuxHocDL@users.noreply.github.com>
2024-05-26 07:21:23 +0700

    Update interact.py

--- interact.py
+++ interact.py
@@ -1,9 +1,19 @@
 class Interact(): 
     # Hàm kiểm tra va chạm
-    def check_collision(rect1, rect2):
+   def check_collision(rect1, rect2):
      if rect1.colliderect(rect2):
         intersection = rect1.clip(rect2)
         return (intersection.x, intersection.y)
      else:
         return None
+     
+   @staticmethod
+   def check_collision_2(rect1, rect2):
+        if rect1.colliderect(rect2):
+            y_difference = abs(rect1.y - rect2.y)
+            if y_difference <= 50:
+                return True
+        else:
+            return False
+       
      
